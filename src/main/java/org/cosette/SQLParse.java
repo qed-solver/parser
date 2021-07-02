8910682b65cb038eed5cfa1f0566f1884670c675
Sicheng <pansicheng@berkeley.edu>
2021-07-02 17:25:40 +0800

    Minimal working example of Calcite.

--- src/main/java/org/cosette/SQLParse.java
+++ src/main/java/org/cosette/SQLParse.java
@@ -1,8 +1,14 @@
 package org.cosette;
 
+import org.apache.calcite.adapter.java.ReflectiveSchema;
+import org.apache.calcite.config.CalciteConnectionProperty;
 import org.apache.calcite.config.Lex;
+import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.calcite.prepare.PlannerImpl;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.externalize.RelJsonWriter;
+import org.apache.calcite.schema.SchemaPlus;
+import org.apache.calcite.server.ServerDdlExecutor;
 import org.apache.calcite.sql.*;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.parser.SqlParseException;
@@ -13,36 +19,50 @@ import org.apache.calcite.sql.validate.SqlConformanceEnum;
 import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.RelBuilder;
+import org.apache.calcite.tools.ValidationException;
 
+import java.io.StringReader;
+import java.sql.*;
 import java.util.Arrays;
+import java.util.Properties;
 
 public class SQLParse {
-    public static void main( String[] args ) throws SqlParseException
-    {
-        String sql = """
-                CREATE TABLE R(A INTEGER)""";
-        SqlParser.Config sqlParserConfig = SqlParser.configBuilder()
-                .setParserFactory(SqlDdlParserImpl.FACTORY)
-                .setConformance(SqlConformanceEnum.MYSQL_5)
-                .setLex(Lex.MYSQL)
-                .build();
 
-        SqlParser sqlParser = SqlParser.create(sql, sqlParserConfig);
-        SqlNode sqlQuery = sqlParser.parseQuery();
-        System.out.println(sqlQuery);
-        FrameworkConfig config = Frameworks.newConfigBuilder().build();
-        final RelBuilder builder = RelBuilder.create(config);
-        final RelNode node = builder
-                .scan("EMP")
-                .aggregate(builder.groupKey("DEPTNO"),
-                        builder.count(false, "C"),
-                        builder.sum(false, "S", builder.field("SAL")))
-                .filter(
-                        builder.call(SqlStdOperatorTable.GREATER_THAN,
-                                builder.field("C"),
-                                builder.literal(10)))
-                .build();
-        node.explain(new RelJsonWriter());
+    public static void main( String[] args )
+            throws ClassNotFoundException, SQLException, SqlParseException, ValidationException
+    {
+        Class.forName("org.apache.calcite.jdbc.Driver");
+        Properties info = new Properties();
+        info.setProperty(CalciteConnectionProperty.PARSER_FACTORY.camelName(), ServerDdlExecutor.class.getName() + "#PARSER_FACTORY");
+        info.setProperty(CalciteConnectionProperty.MATERIALIZATIONS_ENABLED.camelName(), "true");
+        info.setProperty(CalciteConnectionProperty.FUN.camelName(), "standard,oracle");
+        Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
+        CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);
+        Statement statement = calciteConnection.createStatement();
+
+        statement.execute("""
+                              CREATE TABLE EMP (
+                             	EMP_ID INTEGER,
+                             	EMP_NAME CHARACTER,
+                             	DEPT_ID INTEGER
+                             )
+                             """);
+
+        statement.execute("""
+                            CREATE TABLE DEPT (
+                                DEPT_ID INTEGER,
+                                DEPT_NAME CHARACTER 
+                            )
+                            """);
+
+        ResultSet result = statement.executeQuery("""
+                            SELECT * FROM
+                            (SELECT * FROM EMP WHERE DEPT_ID = 10) AS T
+                            WHERE T.DEPT_ID + 5 > T.EMP_ID
+                            """);
+
+        statement.close();
+
     }
 }
 
