4c7a22c4f830cbb4a4440e49813709f720ccb47b
Sicheng <pansicheng@berkeley.edu>
2021-07-02 17:25:40 +0800

    Minimal working example of Calcite.

--- src/main/java/org/cosette/SQLParse.java
+++ src/main/java/org/cosette/SQLParse.java
@@ -1,49 +1,93 @@
 package org.cosette;
 
-import org.apache.calcite.config.Lex;
+import org.apache.calcite.config.CalciteConnectionProperty;
+import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.calcite.prepare.PlannerImpl;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.externalize.RelJsonWriter;
+import org.apache.calcite.schema.SchemaPlus;
+import org.apache.calcite.server.ServerDdlExecutor;
 import org.apache.calcite.sql.*;
-import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.parser.SqlParseException;
-import org.apache.calcite.sql.parser.SqlParser;
-import org.apache.calcite.sql.parser.ddl.SqlDdlParserImpl;
 import org.apache.calcite.sql.util.SqlVisitor;
-import org.apache.calcite.sql.validate.SqlConformanceEnum;
 import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
-import org.apache.calcite.tools.RelBuilder;
+import org.apache.calcite.tools.ValidationException;
 
+import java.io.IOException;
+import java.sql.*;
 import java.util.Arrays;
+import java.util.Properties;
 
 public class SQLParse {
-    public static void main( String[] args ) throws SqlParseException
-    {
-        String sql = """
-                CREATE TABLE R(A INTEGER)""";
-        SqlParser.Config sqlParserConfig = SqlParser.configBuilder()
-                .setParserFactory(SqlDdlParserImpl.FACTORY)
-                .setConformance(SqlConformanceEnum.MYSQL_5)
-                .setLex(Lex.MYSQL)
-                .build();
 
-        SqlParser sqlParser = SqlParser.create(sql, sqlParserConfig);
-        SqlNode sqlQuery = sqlParser.parseQuery();
-        System.out.println(sqlQuery);
-        FrameworkConfig config = Frameworks.newConfigBuilder().build();
-        final RelBuilder builder = RelBuilder.create(config);
-        final RelNode node = builder
-                .scan("EMP")
-                .aggregate(builder.groupKey("DEPTNO"),
-                        builder.count(false, "C"),
-                        builder.sum(false, "S", builder.field("SAL")))
-                .filter(
-                        builder.call(SqlStdOperatorTable.GREATER_THAN,
-                                builder.field("C"),
-                                builder.literal(10)))
-                .build();
-        node.explain(new RelJsonWriter());
+    public static void main( String[] args ) throws SQLException, SqlParseException, ValidationException, IOException {
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        schemaGenerator.applyDDL("""
+                                  CREATE TABLE EMP (
+                                    EMP_ID INTEGER,
+                                    EMP_NAME CHARACTER,
+                                    DEPT_ID INTEGER
+                                 )
+                             """);
+        schemaGenerator.applyDDL("""
+                                CREATE TABLE DEPT (
+                                    DEPT_ID INTEGER,
+                                    DEPT_NAME CHARACTER 
+                                )
+                                """);
+
+        String sql = """
+                    SELECT * FROM EMP WHERE EXISTS (SELECT * FROM DEPT WHERE EMP.DEPT_ID = DEPT.DEPT_ID)                  
+                    """;
+
+        PlannerImpl planner = schemaGenerator.createPlanner();
+        schemaGenerator.close();
+
+        SqlNode sqlNode = planner.parse(sql);
+
+        planner.validate(sqlNode);
+
+        RelNode relNode = planner.convert(sqlNode);
+        RelJsonWriter relJsonWriter = new RelJsonWriter();
+        relNode.explain(relJsonWriter);
+//        System.out.println(relJsonWriter.asString());
     }
+}
+
+class SchemaGenerator {
+
+    private final CalciteConnection calciteConnection;
+
+    public SchemaGenerator() throws SQLException {
+        Properties info = new Properties();
+        info.setProperty(CalciteConnectionProperty.PARSER_FACTORY.camelName(), ServerDdlExecutor.class.getName() + "#PARSER_FACTORY");
+        info.setProperty(CalciteConnectionProperty.MATERIALIZATIONS_ENABLED.camelName(), "true");
+        info.setProperty(CalciteConnectionProperty.FUN.camelName(), "standard,oracle");
+        Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
+        calciteConnection = connection.unwrap(CalciteConnection.class);
+    }
+
+    public void applyDDL(String ddl) throws SQLException {
+        Statement statement = calciteConnection.createStatement();
+        statement.execute(ddl);
+        statement.close();
+    }
+
+    public SchemaPlus extractSchema() {
+        return calciteConnection.getRootSchema();
+    }
+
+    public PlannerImpl createPlanner() {
+        FrameworkConfig config = Frameworks.newConfigBuilder()
+                .defaultSchema(extractSchema())
+                .build();
+        return new PlannerImpl(config);
+    }
+
+    public void close() throws SQLException { calciteConnection.close(); }
+
 }
 
 class SQLVisitorJSON implements SqlVisitor<Void> {
