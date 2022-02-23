5b7f4dfb2303b4ae1f457936b510bb2e8dfb6948
Louis5499 <louisby0123@gmail.com>
2022-02-22 16:32:34 -0800

    merge codes & fix

--- test/data-100/test_custom.rkt
+++ test/data-100/test_custom.rkt
@@ -4,37 +4,16 @@
 
 (define INDIV_SAMPLE_NYC (Table "INDIV_SAMPLE_NYC" (list "CMTE_ID" "TRANSACTION_AMT" "NAME") (gen-sym-schema 3 1)))
 
-<<<<<<< HEAD
-(define (gen-a)
-	(define-symbolic* a boolean?)
-  a)
-(define (gen-b)
-	(define-symbolic* b boolean?)
-  b)
-(define (gen-c)
-	(define-symbolic* c boolean?)
-  c)
-=======
 
-(define q1s (SELECT (VALS "INDIV_SAMPLE_NYC.CMTE_ID" "INDIV_SAMPLE_NYC.TRANSACTION_AMT" "INDIV_SAMPLE_NYC.NAME") FROM (NAMED INDIV_SAMPLE_NYC) WHERE (NOT (BINOP "INDIV_SAMPLE_NYC.CMTE_ID" = "INDIV_SAMPLE_NYC.TRANSACTION_AMT") )))
->>>>>>> group_by
+(define q1s (SELECT (VALS "INDIV_SAMPLE_NYC.CMTE_ID" aggr-sum  "INDIV_SAMPLE_NYC.TRANSACTION_AMT") FROM (NAMED INDIV_SAMPLE_NYC) WHERE (TRUE) GROUP-BY (list "INDIV_SAMPLE_NYC.CMTE_ID")))
 
-(define q1s (SELECT (VALS "INDIV_SAMPLE_NYC.CMTE_ID" "INDIV_SAMPLE_NYC.TRANSACTION_AMT" "INDIV_SAMPLE_NYC.NAME") FROM (NAMED INDIV_SAMPLE_NYC) WHERE (AND (filter-sym (gen-a)) (filter-sym (gen-a)) )))
-
-<<<<<<< HEAD
-(define q2s (SELECT (VALS "INDIV_SAMPLE_NYC.CMTE_ID" "INDIV_SAMPLE_NYC.NAME") FROM (NAMED INDIV_SAMPLE_NYC) WHERE (filter-sym (gen-b))))
-=======
-(define q2s (SELECT-GROUP (VALS "INDIV_SAMPLE_NYC.CMTE_ID" "INDIV_SAMPLE_NYC.TRANSACTION_AMT") FROM (NAMED INDIV_SAMPLE_NYC) WHERE (TRUE) '( "INDIV_SAMPLE_NYC.CMTE_ID") aggr-sum "INDIV_SAMPLE_NYC.TRANSACTION_AMT"))
-
-(define q2s (SELECT-GROUP (VALS "INDIV_SAMPLE_NYC.CMTE_ID") FROM (NAMED INDIV_SAMPLE_NYC) WHERE (TRUE) '( "INDIV_SAMPLE_NYC.CMTE_ID")))))
-
->>>>>>> group_by
+(define q2s (SELECT (VALS "INDIV_SAMPLE_NYC.CMTE_ID") FROM (NAMED INDIV_SAMPLE_NYC) WHERE (TRUE) GROUP-BY (list "INDIV_SAMPLE_NYC.CMTE_ID") HAVING (BINOP (VAL-UNOP aggr-sum "INDIV_SAMPLE_NYC.TRANSACTION_AMT") > 10)))
 
 
 (cond
-	[(eq? #f #f) println("LIKE regex does not match")]
-	[(eq? #f #f) println("ORDER BY does not match")]
-	[(eq? #t #f)
+	[(eq? #f #t) println("LIKE regex does not match")]
+	[(eq? #f #t) println("ORDER BY does not match")]
+	[(eq? #t #t)
 		(let* ([model (verify (same q1s q2s))]
 			   [concrete-t1 (clean-ret-table (evaluate INDIV_SAMPLE_NYC model))])
 			(println concrete-t1)
