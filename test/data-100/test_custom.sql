712c0f3c8cafd6f94e104728b6c99970d3a615c1
Louis5499 <louisby0123@gmail.com>
2022-03-02 13:21:32 -0800

    comment

--- test/data-100/test_custom.sql
+++ test/data-100/test_custom.sql
@@ -1,15 +1,9 @@
-CREATE TABLE nyc(    
+CREATE TABLE indiv_sample_nyc(    
 	cmte_id INT,
     transaction_amt INT,
     name VARCHAR(10)
     );
 
-CREATE TABLE sf(    
-	cmte_id INT,
-    tx_amt INT,
-    description VARCHAR(10)
-    );
+SELECT * FROM indiv_sample_nyc WHERE name LIKE '%TRUMP%' AND name LIKE '%TRUMP%' ORDER BY cmte_id DESC;
 
-SELECT * FROM nyc LEFT JOIN sf ON nyc.cmte_id = sf.cmte_id;
-
-SELECT * FROM nyc LEFT JOIN sf ON nyc.cmte_id = sf.cmte_id;
+SELECT cmte_id, name FROM indiv_sample_nyc WHERE name LIKE '%DONALD%';
