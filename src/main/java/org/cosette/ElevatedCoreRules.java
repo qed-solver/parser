651756ee9772b6a0c205a0c1ac52475ec0a7ded5
Macronova <trivial@invariantspace.com>
2023-01-19 13:46:45 -0800

    Optimize code

--- src/main/java/org/cosette/ElevatedCoreRules.java
+++ src/main/java/org/cosette/ElevatedCoreRules.java
@@ -1,15 +1,13 @@
 package org.cosette;
 
 import kala.collection.Seq;
-import kala.control.Option;
 import kala.tuple.Tuple;
 import kala.tuple.Tuple2;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.JoinRelType;
-import org.apache.calcite.sql.SqlOperator;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
+import org.apache.calcite.tools.RelBuilder;
 
-import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Modifier;
 import java.nio.file.Files;
@@ -21,254 +19,248 @@ public class ElevatedCoreRules {
 
     public static Tuple2<RelNode, RelNode> calcMerge() {
         // A Calc is equivalent to a project above a filter
-        RuleBuilder builder = RuleBuilder.create();
-        CosetteTable input = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("INPUT", true), false)));
-        SqlOperator bottomFilter = builder.genericPredicateOp("bottom", true);
-        SqlOperator bottomProject = builder.genericProjectionOp("bottom", new RelType.VarType("INTER", true));
-        SqlOperator topFilter = builder.genericPredicateOp("top", true);
-        SqlOperator topProject = builder.genericProjectionOp("top", new RelType.VarType("RESULT", true));
+        var builder = RuleBuilder.create();
+        var input = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("INPUT", true), false)));
+        var bottomFilter = builder.genericPredicateOp("bottom", true);
+        var bottomProject = builder.genericProjectionOp("bottom", new RelType.VarType("INTER", true));
+        var topFilter = builder.genericPredicateOp("top", true);
+        var topProject = builder.genericProjectionOp("top", new RelType.VarType("RESULT", true));
         builder.addTable(input);
         builder.scan(input.getName());
         builder.filter(builder.call(bottomFilter, builder.fields()));
         builder.project(builder.call(bottomProject, builder.fields()));
         builder.filter(builder.call(topFilter, builder.fields()));
         builder.project(builder.call(topProject, builder.fields()));
-        RelNode before = builder.build();
+        var before = builder.build();
         builder.scan(input.getName());
         builder.filter(builder.call(SqlStdOperatorTable.AND,
                 builder.call(bottomFilter, builder.fields()),
                 builder.call(topFilter, builder.call(bottomProject, builder.fields()))));
         builder.project(builder.call(topProject, builder.call(bottomProject, builder.fields())));
-        RelNode after = builder.build();
+        var after = builder.build();
         return Tuple.of(before, after);
     }
 
     public static Tuple2<RelNode, RelNode> filterIntoJoin() {
-        // TODO: carefully think about join types
-        RuleBuilder builder = RuleBuilder.create();
-        CosetteTable left = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("LEFT", true), false)));
-        CosetteTable right = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("RIGHT", true), false)));
+        var builder = RuleBuilder.create();
+        var left = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("LEFT", true), false)));
+        var right = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("RIGHT", true), false)));
         builder.addTable(left).addTable(right);
         builder.scan(left.getName()).scan(right.getName());
-        SqlOperator joinCondition = builder.genericPredicateOp("join", true);
-        SqlOperator filterCondition = builder.genericPredicateOp("filter", true);
+        var joinCondition = builder.genericPredicateOp("join", true);
+        var filterCondition = builder.genericPredicateOp("filter", true);
         builder.join(JoinRelType.INNER, builder.call(joinCondition, Seq.from(builder.fields(2, 0)).concat(builder.fields(2, 1))));
         builder.filter(builder.call(filterCondition, builder.fields()));
-        RelNode before = builder.build();
+        var before = builder.build();
         builder.scan(left.getName()).scan(right.getName());
-        builder.join(JoinRelType.FULL, builder.call(SqlStdOperatorTable.AND,
+        builder.join(JoinRelType.INNER, builder.call(SqlStdOperatorTable.AND,
                 builder.call(joinCondition, Seq.from(builder.fields(2, 0)).concat(builder.fields(2, 1))),
                 builder.call(filterCondition, Seq.from(builder.fields(2, 0)).concat(builder.fields(2, 1)))));
-        RelNode after = builder.build();
+        var after = builder.build();
         return Tuple.of(before, after);
     }
 
     public static Tuple2<RelNode, RelNode> filterProjectTranspose() {
-        RuleBuilder builder = RuleBuilder.create();
-        CosetteTable input = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("INPUT", true), false)));
+        var builder = RuleBuilder.create();
+        var input = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("INPUT", true), false)));
         builder.addTable(input);
-        SqlOperator project = builder.genericProjectionOp("select", new RelType.VarType("PROJECT", true));
-        SqlOperator filter = builder.genericPredicateOp("filter", true);
+        var project = builder.genericProjectionOp("select", new RelType.VarType("PROJECT", true));
+        var filter = builder.genericPredicateOp("filter", true);
         builder.scan(input.getName()).project(builder.call(project, builder.fields()));
         builder.filter(builder.call(filter, builder.fields()));
-        RelNode before = builder.build();
+        var before = builder.build();
         builder.scan(input.getName()).filter(builder.call(filter, builder.call(project, builder.fields())));
         builder.project(builder.call(project, builder.fields()));
-        RelNode after = builder.build();
+        var after = builder.build();
         return Tuple.of(before, after);
     }
 
-    public static Tuple2<RelNode, RelNode> filterCorrelate() {
+    public static Seq<Tuple2<RelNode, RelNode>> filterCorrelate() {
         return null;
     }
 
-    public static Option<Tuple2<RelNode, RelNode>> filterSetOpTranspose(SqlOperator kind) {
-        RuleBuilder builder = RuleBuilder.create();
-        CosetteTable one = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("SHARED", true), false)));
-        CosetteTable other = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("SHARED", true), false)));
-        SqlOperator filter = builder.genericPredicateOp("filter", true);
-        builder.addTable(one).addTable(other);
-        builder.scan(one.getName()).scan(other.getName());
-        if (kind == SqlStdOperatorTable.UNION_ALL) {
-            builder.union(true);
-        } else if (kind == SqlStdOperatorTable.EXCEPT_ALL) {
-            builder.minus(true);
-        } else if (kind == SqlStdOperatorTable.INTERSECT_ALL) {
-            builder.intersect(true);
-        } else {
-            return Option.none();
+    public static Seq<Tuple2<RelNode, RelNode>> filterSetOpTranspose() {
+        return Seq.of(SqlStdOperatorTable.UNION_ALL, SqlStdOperatorTable.EXCEPT_ALL).map(kind -> {
+            var builder = RuleBuilder.create();
+            var one = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("SHARED", true), false)));
+            var other = builder.createSimpleTable(Seq.of(Tuple.of(new RelType.VarType("SHARED", true), false)));
+            var filter = builder.genericPredicateOp("filter", true);
+            builder.addTable(one).addTable(other);
+            builder.scan(one.getName()).scan(other.getName());
+            if (kind == SqlStdOperatorTable.UNION_ALL) {
+                builder.union(true);
+            } else if (kind == SqlStdOperatorTable.EXCEPT_ALL) {
+                builder.minus(true);
+            }
+            builder.filter(builder.call(filter, builder.fields()));
+            var before = builder.build();
+            builder.scan(one.getName()).filter(builder.call(filter, builder.fields()));
+            builder.scan(other.getName()).filter(builder.call(filter, builder.fields()));
+            if (kind == SqlStdOperatorTable.UNION_ALL) {
+                builder.union(true);
+            } else if (kind == SqlStdOperatorTable.EXCEPT_ALL) {
+                builder.minus(true);
+            }
+            var after = builder.build();
+            return Tuple.of(before, after);
+        });
+    }
+//
+//    public static Tuple2<RelNode, RelNode> projectCorrelateTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> projectFilterTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> projectSubQueryToCorrelate() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> filterSubQueryToCorrelate() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinSubQueryToCorrelate() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> projectToSemiJoin() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> projectJoinRemove() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> projectJoinTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> projectSetOpTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinConditionPush() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinAddRedundantSemiJoin() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinAssociate() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinCommute() {
+//        // Inner/Outer joins
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinExtractFilter() {
+//        // Inner/Outer joins
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinProjectBothTranspose() {
+//        // Inner/Outer joins
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinProjectLeftTranspose() {
+//        // Inner/Outer joins
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinProjectRightTranspose() {
+//        // Inner/Outer joins
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinPushExpressions() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinPushTransitivePredicates() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinDeriveIsNotNullFilter() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinToCorrelate() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinToSemiJoin() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinLeftUnionTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> joinRightUnionTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> semiJoinFilterTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> semiJoinProjectTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> semiJoinJoinTranspose() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> semiJoinRemove() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> unionMerge() {
+//        return null;
+//    }
+//
+//    public static Tuple2<RelNode, RelNode> projectFilterValueMerge() {
+//        return null;
+//    }
+
+    public static void dumpTransformedRule(RelNode before, RelNode after, boolean verbose, Path dumpPath) throws IOException {
+        if (verbose) {
+            System.out.println(">>>>>> " + dumpPath.getFileName().toString() + " <<<<<<");
+            System.out.println("Before:");
+            System.out.println(before.explain());
+            System.out.println("After:");
+            System.out.println(after.explain());
         }
-        builder.filter(builder.call(filter, builder.fields()));
-        RelNode before = builder.build();
-        builder.scan(one.getName()).filter(builder.call(filter, builder.fields()));
-        builder.scan(other.getName()).filter(builder.call(filter, builder.fields()));
-        if (kind == SqlStdOperatorTable.UNION_ALL) {
-            builder.union(true);
-        } else if (kind == SqlStdOperatorTable.EXCEPT_ALL) {
-            builder.minus(true);
-        } else {
-            builder.intersect(true);
-        }
-        RelNode after = builder.build();
-        return Option.some(Tuple.of(before, after));
-    }
-
-    public static Tuple2<RelNode, RelNode> filterUnionTranspose() {
-        return filterSetOpTranspose(SqlStdOperatorTable.UNION_ALL).get();
-    }
-
-    public static Tuple2<RelNode, RelNode> filterExceptTranspose() {
-        return filterSetOpTranspose(SqlStdOperatorTable.EXCEPT_ALL).get();
-    }
-
-    /**
-     * Currently Intersect is not supported by prover
-     * public static Tuple2<RelNode, RelNode> filterIntersectTranspose() {
-     *     return filterSetOpTranspose(SqlStdOperatorTable.INTERSECT_ALL).get();
-     * }
-     */
-
-    public static Tuple2<RelNode, RelNode> projectCorrelateTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> projectFilterTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> projectSubQueryToCorrelate() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> filterSubQueryToCorrelate() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinSubQueryToCorrelate() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> projectToSemiJoin() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> projectJoinRemove() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> projectJoinTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> projectSetOpTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinConditionPush() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinAddRedundantSemiJoin() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinAssociate() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinCommute() {
-        // Inner/Outer joins
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinExtractFilter() {
-        // Inner/Outer joins
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinProjectBothTranspose() {
-        // Inner/Outer joins
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinProjectLeftTranspose() {
-        // Inner/Outer joins
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinProjectRightTranspose() {
-        // Inner/Outer joins
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinPushExpressions() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinPushTransitivePredicates() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinDeriveIsNotNullFilter() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinToCorrelate() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinToSemiJoin() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinLeftUnionTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> joinRightUnionTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> semiJoinFilterTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> semiJoinProjectTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> semiJoinJoinTranspose() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> semiJoinRemove() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> unionMerge() {
-        return null;
-    }
-
-    public static Tuple2<RelNode, RelNode> projectFilterValueMerge() {
-        return null;
+        RelJSONShuttle.dumpToJSON(List.of(before, after), dumpPath.toFile());
     }
 
     public static void dumpElevatedRules(Path dumpFolder, boolean verbose) throws IOException {
         Files.createDirectories(dumpFolder);
-        Seq.of(ElevatedCoreRules.class.getDeclaredMethods())
-                .filter(method -> Modifier.isStatic(method.getModifiers()) && method.getReturnType().getName().equals("kala.tuple.Tuple2"))
+        Seq.of(ElevatedCoreRules.class.getMethods())
+                .filter(method -> Modifier.isStatic(method.getModifiers()))
                 .forEachUnchecked(method -> {
-                    String ruleName = method.getName();
-                    Tuple2<RelNode, RelNode> rewrite = (Tuple2<RelNode, RelNode>) method.invoke(null);
-                    if (verbose) {
-                        System.out.println(">>>>>> " + ruleName + " <<<<<<");
-                        System.out.println("Before:");
-                        System.out.println(rewrite._1.explain());
-                        System.out.println("After:");
-                        System.out.println(rewrite._2.explain());
+                    switch (method.getReturnType().getSimpleName()) {
+                        case "Tuple2" -> {
+                            var ruleName = method.getName();
+                            var rewrite = (Tuple2<RelNode, RelNode>) method.invoke(null);
+                            dumpTransformedRule(rewrite._1, rewrite._2, verbose,
+                                    Paths.get(dumpFolder.toAbsolutePath().toString(), ruleName + ".json"));
+                        }
+                        case "Seq" -> {
+                            var ruleName = method.getName();
+                            var rewrites = (Seq<Tuple2<RelNode, RelNode>>) method.invoke(null);
+                            rewrites.forEachIndexedUnchecked((index, rewrite) -> dumpTransformedRule(rewrite._1, rewrite._2, verbose,
+                                    Paths.get(dumpFolder.toAbsolutePath().toString(), ruleName + index + ".json")));
+                        }
                     }
-                    File dump = Paths.get(dumpFolder.toAbsolutePath().toString(), ruleName + ".json").toFile();
-                    RelJSONShuttle.dumpToJSON(List.of(rewrite._1, rewrite._2), dump);
                 });
     }
 
