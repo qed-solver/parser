bf4445c7fedebe13183b57cca648bfc4479c1ccf
Sicheng Pan <trivial@invariantspace.com>
2023-07-18 15:35:58 -0700

    Use cvc5

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    cvc5-src = {
+      flake = false;
+      url = "github:cvc5/cvc5";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, cvc5-src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cvc5-java = pkgs.stdenv.mkDerivation {
+          name = "cvc5-java";
+          version = "unstable";
+          src = cvc5-src;
+
+          nativeBuildInputs = with pkgs; [ pkg-config cmake flex ];
+
+          buildInputs = with pkgs; [
+            cadical.dev
+            symfpu
+            gmp
+            gtest
+            libantlr3c
+            antlr3_4
+            boost
+            jdk
+            (python3.withPackages (ps: with ps; [ pyparsing toml ]))
+          ];
+
+          cmakeFlags = [
+            "-DBUILD_BINDINGS_JAVA=ON"
+            "-DBUILD_SHARED_LIBS=1"
+            "-DCMAKE_BUILD_TYPE=Production"
+            "-DANTLR3_JAR=${pkgs.antlr3_4}/lib/antlr/antlr-3.4-complete.jar"
+          ];
+
+          preConfigure = ''
+            patchShebangs ./src/
+          '';
+
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cvc5-java
+            jdk
+            jetbrains.idea-community
+          ];
+        };
+      });
+}
