61adc9a592fa01ee2f742924d870c184dcf30f48
wkaiz <wzheng0302@berkeley.edu>
2025-06-03 14:10:11 -0700

    Trying SemiJoinJoinRightTranspose

--- src/main/java/org/qed/Generated/RRuleInstances/SemiJoinJoinTranspose.java
+++ src/main/java/org/qed/Generated/RRuleInstances/SemiJoinJoinTranspose.java
@@ -3,28 +3,21 @@ package org.qed.Generated.RRuleInstances;
 import org.apache.calcite.rel.core.JoinRelType;
 import org.qed.RRule;
 import org.qed.RelRN;
-import org.qed.RexRN;
-
 
 public record SemiJoinJoinTranspose() implements RRule {
     static final RelRN x = RelRN.scan("X", "x_type");
     static final RelRN y = RelRN.scan("Y", "y_type");
     static final RelRN z = RelRN.scan("Z", "z_type");
 
-    static final RexRN innerCond = x.joinPred("inner", y);
-    static final RelRN xyJoin = x.join(JoinRelType.INNER, innerCond, y);
-
-    static final RexRN semiCond = xyJoin.joinPred("semi", z);
-
     @Override
     public RelRN before() {
-        return xyJoin.join(JoinRelType.SEMI, semiCond, z);
+        RelRN joinXY = x.join(JoinRelType.INNER, x.joinPred("inner", y), y);
+        return joinXY.join(JoinRelType.SEMI, joinXY.joinPred("semi", z), z);
     }
 
     @Override
     public RelRN after() {
-        RelRN xzSemiJoin = x.join(JoinRelType.SEMI, x.joinPred("semi", z), z);
-        return xzSemiJoin.join(JoinRelType.INNER, xzSemiJoin.joinPred("inner", y), y);
+        RelRN semiJoinYZ = y.join(JoinRelType.SEMI, y.joinPred("semi", z), z);
+        return x.join(JoinRelType.INNER, x.joinPred("inner", semiJoinYZ), semiJoinYZ);
     }
 }
-
